4ae2982c8d706a5193ee68009401113abf2c81b8
deswitasy <deswitas06@gmail.com>
2025-03-26 08:16:19 +0700

    penambahan design by contract pada semua kelas

--- jurnalmodul6/Program.cs
+++ jurnalmodul6/Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
 class SayaTubeUser
 {
     private int id;
@@ -7,8 +9,8 @@ class SayaTubeUser
 
     public SayaTubeUser(string Username)
     {
-        Random random = new Random();
-        this.id = new Random().Next(10000, 99999);
+        Debug.Assert(Username != null, "Username tidak boleh null");
+        Debug.Assert(Username.Length <= 100, "Usernametidak boleh lebih dari 100 karakter");
         this.Username = Username;
         this.uploadedVideos = new List<SayaTubeVideo>();
     }
@@ -23,11 +25,8 @@ class SayaTubeUser
     }
     public void AddVideo(SayaTubeVideo video)
     {
-        if (video == null)
-        {
-            Console.WriteLine("Video tidak boleh kosong");
-            return;
-        }
+        Debug.Assert(video != null, "video yang ditambah tidak boleh null");
+        Debug.Assert(video.GetPlayCount() <= int.MaxValue, "play count melibihi batas integer maksimum");
         uploadedVideos.Add(video);
     }
     public void PrintAllVideoPlaycount()
@@ -48,6 +47,8 @@ class SayaTubeVideo
 
     public SayaTubeVideo(string title)
     {
+        Debug.Assert(title != null, "Judul video tidak boleh null");
+        Debug.Assert(title.Length <= 200, "Judul video tidak boleh lebih dari 200 karakter"); 
         Random random = new Random();
         this.id = new Random().Next(10000, 100000);
         this.title = title;
@@ -55,12 +56,19 @@ class SayaTubeVideo
     }
     public void IncreasePlayCount(int count)
     {
-        if (count < 0)
+        Debug.Assert(count > 0, "play count harus lebih dari nol");
+        Debug.Assert(count <= 25000000, "play count maksimal 25.000.000");
+        try
         {
-            Console.WriteLine("Jumlah play count tidak boleh negatif");
-            return;
+            checked
+            {
+                this.playCount += count;
+            }
         }
-        playCount += count;
+        catch (OverflowException)
+        {
+            Console.WriteLine("Terjadi overflow pada penambahan play count");
+        }
     }
     public void PrintVideoDetails()
     {
@@ -82,19 +90,33 @@ class Program
     static void Main(string[] args)
     {
         SayaTubeUser user = new SayaTubeUser("Deswita");
-        user.AddVideo(new SayaTubeVideo("Review Film Moana 2 oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Harry Potter oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Fantastic Beasts oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Sherlock Holmes oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Enola Holmes oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Avengers oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Barbie oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Batman oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film John Wick oleh Deswita"));
-        user.AddVideo(new SayaTubeVideo("Review Film Extraction oleh Deswita"));
+        SayaTubeVideo video1 = new SayaTubeVideo("Review Film Moana 2 oleh Deswita");
+        SayaTubeVideo video2 = new SayaTubeVideo("Review Film Harry Potter oleh Deswita");
+        SayaTubeVideo video3 = new SayaTubeVideo("Review Film Fantastic Beasts oleh Deswita");
+        SayaTubeVideo video4 = new SayaTubeVideo("Review Film Sherlock Holmes oleh Deswita");
+        SayaTubeVideo video5 = new SayaTubeVideo("Review Film Enola Holmes oleh Deswita");
+        SayaTubeVideo video6 = new SayaTubeVideo("Review Film Avengers oleh Deswita");
+        SayaTubeVideo video7= new SayaTubeVideo("Review Film Barbie oleh Deswita");
+        SayaTubeVideo video8 = new SayaTubeVideo("Review Film Batman oleh Deswita");
+        SayaTubeVideo video9 = new SayaTubeVideo("Review Film John Wick oleh Deswita");
+        SayaTubeVideo video10 = new SayaTubeVideo("Review Film Extraction oleh Deswita");
 
+        user.AddVideo(video1);
+        user.AddVideo(video2);
+        user.AddVideo(video3);
+        user.AddVideo(video4);
+        user.AddVideo(video5);
+        user.AddVideo(video6);
+        user.AddVideo(video7);
+        user.AddVideo(video8);
+        user.AddVideo(video9);
+        user.AddVideo(video10);
+
+        video1.IncreasePlayCount(100);
+        video2.IncreasePlayCount(25000000);
+        video3.IncreasePlayCount(1);
+        video4.IncreasePlayCount(int.MaxValue);
         user.PrintAllVideoPlaycount();
         Console.WriteLine("Total Play Count: " + user.GetTotalVideoPlayCount());
-        Console.ReadLine();
     }
 }
