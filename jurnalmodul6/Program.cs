746c378bff99b0815515bcdefa459314e8a2cb0b
deswitasy <deswitas06@gmail.com>
2025-03-26 07:47:10 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

--- jurnalmodul6/Program.cs
+++ jurnalmodul6/Program.cs
@@ -1,2 +1,100 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+class SayaTubeUser
+{
+    private int id;
+    private List<SayaTubeVideo> uploadedVideos;
+    private string Username;
+
+    public SayaTubeUser(string Username)
+    {
+        Random random = new Random();
+        this.id = new Random().Next(10000, 99999);
+        this.Username = Username;
+        this.uploadedVideos = new List<SayaTubeVideo>();
+    }
+    public int GetTotalVideoPlayCount()
+    {
+        int totalPlayCount = 0;
+        foreach (var video in uploadedVideos)
+        {
+            totalPlayCount += video.GetPlayCount();
+        }
+        return totalPlayCount;
+    }
+    public void AddVideo(SayaTubeVideo video)
+    {
+        if (video == null)
+        {
+            Console.WriteLine("Video tidak boleh kosong");
+            return;
+        }
+        uploadedVideos.Add(video);
+    }
+    public void PrintAllVideoPlaycount()
+    {
+        Console.WriteLine("User: " + this.Username);
+        for(int i = 0; i < uploadedVideos.Count; i++)
+        {
+            Console.WriteLine("Video " + (i + 1) + " judul: " + uploadedVideos[i].GetTitle());
+        }
+    }
+}
+
+class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random random = new Random();
+        this.id = new Random().Next(10000, 100000);
+        this.title = title;
+        this.playCount = 0;
+    }
+    public void IncreasePlayCount(int count)
+    {
+        if (count < 0)
+        {
+            Console.WriteLine("Jumlah play count tidak boleh negatif");
+            return;
+        }
+        playCount += count;
+    }
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID     : " + id);
+        Console.WriteLine("Video Title  : " + title);
+        Console.WriteLine("Play Count   : " + playCount);
+    }
+    public int GetPlayCount()
+    {
+        return this.playCount;
+    }
+    public string GetTitle()
+    {
+        return this.title;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeUser user = new SayaTubeUser("Deswita");
+        user.AddVideo(new SayaTubeVideo("Review Film Moana 2 oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Harry Potter oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Fantastic Beasts oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Sherlock Holmes oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Enola Holmes oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Avengers oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Barbie oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Batman oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film John Wick oleh Deswita"));
+        user.AddVideo(new SayaTubeVideo("Review Film Extraction oleh Deswita"));
+
+        user.PrintAllVideoPlaycount();
+        Console.WriteLine("Total Play Count: " + user.GetTotalVideoPlayCount());
+        Console.ReadLine();
+    }
+}
--- jurnalmodul6/obj/Debug/net8.0/jurnalmodul6.AssemblyInfo.cs
+++ jurnalmodul6/obj/Debug/net8.0/jurnalmodul6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jurnalmodul6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b9b13eecc42ab8dda33452ae14c53ea7d7cdc03")]
 [assembly: System.Reflection.AssemblyProductAttribute("jurnalmodul6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jurnalmodul6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
